b06b1f8f66561f79c24c35b3a8b029296282c08b
Zerofelx <zeroflex3@gmail.com>
2020-01-12 23:34:56 -0500

    Agregando proyectos al portafolio, estilos y animaciones

---
+++ assets/js/Proyect.js
@@ -0,0 +1,56 @@
+var ShowBehemoth = false
+var ShowGame2D = false
+var ShowCyrozExchange = false
+
+function Show(id) {
+    debugger
+    switch (id) {
+        case "Behemoth":
+            if (ShowBehemoth) {
+                Remove(id)
+                ShowBehemoth = false
+                break
+            } else {
+                Add(id)
+                ShowBehemoth = true
+                break
+            }
+        case "Game2D":
+            if (ShowGame2D) {
+                Remove(id)
+                ShowGame2D = false
+                break
+            } else {
+                Add(id)
+                ShowGame2D = true
+                break
+            }
+        case "CyrozExchange":
+            if (ShowCyrozExchange) {
+                Remove(id)
+                ShowCyrozExchange = false
+                break
+            } else {
+                Add(id)
+                ShowCyrozExchange = true
+                break
+            }
+    }
+    debugger
+}
+
+function Add(id) {
+    var e = document.getElementById(id)
+    e.classList.toggle("show")
+    setTimeout(() => {
+        e.classList.toggle("showP")
+    }, 1000);
+}
+
+function Remove(id) {
+    var e = document.getElementById(id)
+    setTimeout(() => {
+        e.classList.toggle("showP")
+        e.classList.toggle("show")
+    }, 100);
+}
